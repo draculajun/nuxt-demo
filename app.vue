ac7a7fa747461985c80710340eba6b147f4ef844
draculajun <34988153@qq.com>
2024-07-22 17:07:31 +0800

    init

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Hello from App.vue</h1>
+    <h2>color</h2>
+    <el-button>ok</el-button>
   </div>
 </template>
+
+<script setup lang="ts">
+
+const runtimeConfig = useRuntimeConfig();
+console.log(runtimeConfig);
+
+</script>
+
+<style lang="scss" scoped>
+h2 {
+  color: $globalColor;
+}
+</style>
